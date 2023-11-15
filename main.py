767d206556feff0fa72105d124a60b094ab0cac2
Ixonblitz-MatOS <mgavell24@students.uppercapetech.org>
2023-11-15 14:35:42 -0500

    Working on Learn/Flashcards

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import sys,glob
+import typing
 from PyQt5 import QtGui, QtCore
 from os.path import exists
+from platform import system
 from os import remove,mkdir
-from PyQt5.QtWidgets import QApplication,QStackedWidget,QAbstractItemView, QWidget, QLabel, QPushButton,QBoxLayout,QLineEdit,QShortcut,QTableWidget,QTableWidgetItem
+from win32com.client import Dispatch
+from PyQt5.QtWidgets import QApplication,QFrame,QVBoxLayout,QCheckBox,QStackedWidget,QAbstractItemView, QWidget, QLabel, QPushButton,QBoxLayout,QLineEdit,QShortcut,QTableWidget,QTableWidgetItem,QProgressBar,QHBoxLayout
 #make an error dialog box with init parameter of string to put a message to dialog and a button to close it labeled with "okay"
 QFont=QtGui.QFont
 QEventLoop=QtCore.QEventLoop
 Qt=QtCore.Qt
+class TTS:
+    def __init__(self,message:str)->None:
+        self.msg=message
+        self.speaker=Dispatch("SAPI.SpVoice")
+    def read(self)->None:
+        self.speaker.Speak(self.msg)
 class ErrorDialog(QWidget):
     def __init__(self,message:str):
         super().__init__()
@@ -76,6 +85,94 @@ class AskStringDialog(QWidget):
         self.close()
     def get(self)->str:
         return self.final
+class CheckboxFrame(QFrame):
+    def __init__(self, options):
+        super().__init__()
+        self.setStyleSheet("background-color: black; color:white;")
+        self.layout = QVBoxLayout()
+        self.checkboxes = []
+        for option in options:
+            checkbox = QCheckBox(option)
+            checkbox.setStyleSheet("QCheckBox{font: 20pt Comic Sans MS; color: white;border-style: outset; border-width: 2px; border-radius: 10px; border-color: white;} QCheckBox::indicator:checked{background-image:check.jpg;}")
+            self.checkboxes.append(checkbox)
+            self.layout.addWidget(checkbox)
+        self.setLayout(self.layout)
+
+    def get_checked_options(self)->list:
+        checked_options = []
+        for checkbox in self.checkboxes:
+            if checkbox.isChecked():
+                checked_options.append(checkbox.text())
+        return checked_options
+class LearnSettingsDialog(QWidget):
+    def __init__(self,parent) -> None:
+        """
+        Learn Settings:
+        Question Type Choice:
+                Flashcard
+                Multiple Choice
+                Written
+                Read Aloud
+                (All/some/one) allowed
+            Card Order:
+                shuffle=True/False
+            Answer with:
+                Term
+                Definition
+                (Both/One) allowed
+        Storage:
+            {Question Type Choice: [Flashcard, Multiple Choice, Written, Read Aloud], Card Order: [Shuffle], Answer with: [Term, Definition]}
+        """
+        super().__init__()
+        self.p=parent
+        self.layout=QBoxLayout(QBoxLayout.TopToBottom)
+        self.layout.setSpacing(3)
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        #add a keypress event to press the enter button
+        self.keyPressEvent=QShortcut("Return",self)
+        self.keyPressEvent.activated.connect(self._finish)
+        QShortcut("Ctrl+Q",self).activated.connect(self._exit)
+        self.setStyleSheet("background-color: black; color:white;")
+        self.setGeometry(100,100,500,500)
+        self.setLayout(self.layout)
+        self.setWindowTitle("Learn Settings")
+        self.message="Learn Settings"
+        self.label=QLabel(self.message)
+        self.label.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.layout.addWidget(self.label)
+        self.QuestionOptionsLabel=QLabel("Question Type Choice:")
+        self.QuestionOptionsLabel.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.layout.addWidget(self.QuestionOptionsLabel)
+        options = ["Flashcard", "Multiple Choice", "Written", "Read Aloud"]
+        self.checkbox_frame = CheckboxFrame(options)
+        self.layout.addWidget(self.checkbox_frame)
+        self.CardOrderLabel=QLabel("Card Order:")
+        self.CardOrderLabel.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.layout.addWidget(self.CardOrderLabel)
+        options = ["Shuffle"]
+        self.checkbox_frame = CheckboxFrame(options)
+        self.layout.addWidget(self.checkbox_frame)
+        self.AnswerOptionsLabel=QLabel("Answer with:")
+        self.AnswerOptionsLabel.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.layout.addWidget(self.AnswerOptionsLabel)
+        options = ["Term","Definition"]
+        self.checkbox_frame = CheckboxFrame(options)
+        self.layout.addWidget(self.checkbox_frame)
+        self.closeButton=QPushButton("Okay")
+        self.closeButton.setStyleSheet("QPushButton{font: 20pt Comic Sans MS; color: white;border-style: outset; border-width: 2px; border-radius: 10px;border-color: white;} QPushButton::hover{background-color: white; color: black;}")
+        self.closeButton.clicked.connect(self._finish)
+        self.layout.addWidget(self.closeButton)
+        self.layout.addStretch()
+    def _getSettings(self)->dict:
+        settings={}
+        settings["Question Type Choice"]=self.checkbox_frame.get_checked_options()
+        settings["Card Order"]=self.checkbox_frame.get_checked_options()
+        settings["Answer with"]=self.checkbox_frame.get_checked_options()
+        return settings
+    def _finish(self)->None:
+        self.p.LearnSettings=self._getSettings()
+        self.close()
+    def _exit(self):self.p._exit()
 class AskYesNoDialog(QWidget):
     def __init__(self,message)->None:
         super().__init__()
@@ -174,6 +271,38 @@ class StudySet:
                 self._isEmpty=True
             return
         else:return#unknown
+class LearnCard(QWidget):
+    """
+    Used for learn with implemented fade in/out QAnimation
+    """
+    def _learnSubmit(self):
+        """
+        This is run when submit is clicked. We must add an enter Shortcut to press the submit button
+        This function evaluates the answer and changes the text of the button to Correct or Incorrect and changes the background to green or red
+        Then get the next Question and Answer and change the text of the question and answer
+        
+        
+        """
+    def __init__(self,parent) -> None:
+        super().__init__()
+        self.Parent=parent
+        self.learnRightPanelLayout=QBoxLayout(QBoxLayout.TopToBottom,self)
+        self.setLayout(self.learnRightPanelLayout)
+        self.setStyleSheet("background-color: black; color:white;border-style: outset; border-width: 2px; border-radius: 10px;border-color: white;")
+        self.learnRightPanelTitle=QLabel("Question")
+        self.learnRightPanelTitle.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.learnRightPanelLayout.addWidget(self.learnRightPanelTitle)
+        self.learnRightPanelQuestion=QLabel("What is the definition of the term?")
+        self.learnRightPanelQuestion.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.learnRightPanelLayout.addWidget(self.learnRightPanelQuestion)
+        self.learnRightPanelAnswer=QLineEdit("")
+        self.learnRightPanelAnswer.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.learnRightPanelLayout.addWidget(self.learnRightPanelAnswer)
+        self.learnRightPanelSubmitButton=QPushButton("Submit")
+        self.learnRightPanelSubmitButton.setStyleSheet("QPushButton{font: 20pt Comic Sans MS; color: white;} QPushButton::hover{background-color: white; color: black;}")
+        self.learnRightPanelSubmitButton.clicked.connect(self._learnSubmit)
+        self.learnRightPanelLayout.addWidget(self.learnRightPanelSubmitButton)
+
 class MainWindow(QStackedWidget):
     class CustomRemovePopup(QWidget):
         def __init__(self,parent:'MainWindow',e=None) -> None:
@@ -236,6 +365,7 @@ class MainWindow(QStackedWidget):
         self.FirstMenuQWidget=QWidget()
         self.addWidget(self.FirstMenuQWidget)   
         self.setCurrentWidget(self.FirstMenuQWidget)
+        self.LearnSettings={}
         self.firstStudyStudio=True
         self.startlayoutStyle="background-color: black; color:white;"
         # Set up the layout
@@ -293,8 +423,125 @@ class MainWindow(QStackedWidget):
 #:TODO: Make a learn system that allows you to customize term/definition with multiple choice/written/read aloud to learn flashcards
 #:TODO: Make a test system that allows you to customize term/definition with multiple choice/written/read aloud to test flashcards in a test form format
     def currentset(self)->StudySet:return self.currentSet
-    def Flashcards(self):pass
-    def Learn(self):pass
+    def Flashcards(self):
+        """
+        Making flashcard system:
+            using QPushButtons we can use large text with margins and padding to allow for a rounded card experience
+            when you click on the card, it flips to the back
+            when you click on the back, it flips to the front
+            when you click on the next button, it goes to the next card
+            when you click on the previous button, it goes to the previous card
+            when you click on the exit button, it goes back to the study set menu
+            All key binds are inherited from the main menu
+        """
+        self.FlashcardsObj=QWidget()
+        self.FlashcardsLayout=QBoxLayout(QBoxLayout.TopToBottom,self)
+        self.FlashcardsObj.setLayout(self.FlashcardsLayout)
+        self.addWidget(self.FlashcardsObj)
+        self.setCurrentWidget(self.FlashcardsObj)
+        self.FlashcardsLayout.setObjectName("FlashcardsLayout")
+        self.backButton=QPushButton("<---")
+        self.backButton.setStyleSheet("QPushButton{font: 20pt Comic Sans MS; color: white;border-style: outset; border-width: 2px; border-radius: 10px;border-color: white;} QPushButton::hover{background-color: white; color: black;}")
+        self.backButton.clicked.connect(self.StudyStudio)
+        self.backButton.move(0,0)
+        self.FlashcardsLayout.addWidget(self.backButton)
+        #Create a frame that covers the screen where the left side has a correct/incorrect info section with a bar and the buttons for settings and in the middle another frame where flashcards go and buttons below it for next and previous
+        self.flashcardFrame=QWidget()
+        self.flashcardFrameLayout=QBoxLayout(QBoxLayout.LeftToRight,self)
+        self.flashcardFrame.setLayout(self.flashcardFrameLayout)
+        self.FlashcardsLayout.addWidget(self.flashcardFrame)
+        self.flashcardFrame.setObjectName("flashcardFrame")
+        self.flashcardFrame.setStyleSheet("background-color: black; color:white;border-style: outset; border-width: 2px; border-radius: 10px;border-color: white;")
+        self.flashcardLeftPanel=QWidget()
+        self.flashcardLeftPanelLayout=QBoxLayout(QBoxLayout.TopToBottom,self)
+        self.flashcardLeftPanel.setLayout(self.flashcardLeftPanelLayout)
+        self.flashcardFrameLayout.addWidget(self.flashcardLeftPanel)
+        self.flashcardLeftPanel.setObjectName("flashcardLeftPanel")
+        self.flashcardLeftPanel.setStyleSheet("background-color: black; color:white;border-style: outset; border-width: 2px; border-radius: 10px;border-color: white;")
+    def _showLearnSettings(self):
+        self.learnSettingsDialog=LearnSettingsDialog(self)
+        self.learnSettingsDialog.show()
+    def Learn(self):
+        """
+        Using a similar system to quizlet we can use multiple choice, written, read aloud, and flashcard to learn the study set
+        flashcard:
+            same as flashcard mode
+        multiple choice:
+            show the term and 4 definitions and you have to choose the correct definition
+            clicking the definition will change the text to "Correct!" or "Incorrect" and change the background of the button to red or green
+        written:
+            Given the term or definition write the definition or term accordingly
+
+        read aloud:
+
+        global settings:
+            Question Type Choice:
+                Flashcard
+                Multiple Choice
+                Written
+                Read Aloud
+                (All/some/one) allowed
+            Card Order:
+                shuffle=True/False
+            Answer with:
+                Term
+                Definition
+                (Both/One) allowed
+            Card Alerts:Off
+        """
+###########################################################################################################################
+########GUI ONLY###########################################################################################################
+###########################################################################################################################
+        self.LearnObj=QWidget()
+        self.LearnLayout=QBoxLayout(QBoxLayout.TopToBottom,self)
+        self.LearnObj.setLayout(self.LearnLayout)
+        self.addWidget(self.LearnObj)
+        self.setCurrentWidget(self.LearnObj)
+        self.LearnLayout.setObjectName("LearnLayout")
+        self.backButton=QPushButton("<---")
+        self.backButton.setStyleSheet("QPushButton{font: 20pt Comic Sans MS; color: white;border-style: outset; border-width: 2px; border-radius: 10px;border-color: white;} QPushButton::hover{background-color: white; color: black;}")
+        self.backButton.clicked.connect(self.StudyStudio)
+        self.backButton.move(0,0)
+        self.LearnLayout.addWidget(self.backButton)
+        #Create a frame that covers the screen where the left side has a correct/incorrect info section with a bar and the buttons for settings and in the middle another frame where flashcards go and buttons below it for next and previous
+        self.learnFrame=QWidget()
+        self.learnFrameLayout=QBoxLayout(QBoxLayout.LeftToRight,self)
+        self.learnFrame.setLayout(self.learnFrameLayout)
+        self.LearnLayout.addWidget(self.learnFrame)
+        self.learnFrame.setObjectName("learnFrame")
+        self.learnFrame.setStyleSheet("background-color: black; color:white;")
+        self.learnLeftPanel=QWidget()
+        self.learnLeftPanelLayout=QBoxLayout(QBoxLayout.TopToBottom,self)
+        self.learnLeftPanel.setLayout(self.learnLeftPanelLayout)
+        self.learnFrameLayout.addWidget(self.learnLeftPanel)
+        self.learnLeftPanel.setObjectName("learnLeftPanel")
+        self.learnLeftPanel.setStyleSheet("background-color: black; color:white;")
+        self.learnLeftPanelTop=QWidget()
+        self.learnLeftPanelTopLayout=QBoxLayout(QBoxLayout.TopToBottom,self)
+        self.learnLeftPanelTop.setLayout(self.learnLeftPanelTopLayout)
+        self.learnLeftPanelTitle=QLabel("Correct:")
+        self.learnLeftPanelTopLayout.addWidget(self.learnLeftPanelTitle)
+        self.learnLeftPanelTitle.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.learnLeftPanelTopLayout.addWidget(self.learnLeftPanelTitle)
+        self.learnLeftPanelProgressCorrect=QProgressBar()
+        self.learnLeftPanelProgressCorrect.setStyleSheet("font: 20pt Comic Sans MS; color: green;")
+        self.learnLeftPanelTopLayout.addWidget(self.learnLeftPanelProgressCorrect)
+        self.learnLeftPanelTitle=QLabel("Incorrect:")
+        self.learnLeftPanelTopLayout.addWidget(self.learnLeftPanelTitle)
+        self.learnLeftPanelTitle.setStyleSheet("font: 20pt Comic Sans MS; color: white;")
+        self.learnLeftPanelProgressIncorrect=QProgressBar()
+        self.learnLeftPanelProgressIncorrect.setStyleSheet("font: 20pt Comic Sans MS; color: red;")
+        self.learnLeftPanelTopLayout.addWidget(self.learnLeftPanelProgressIncorrect)
+        self.learnLeftPanelLayout.addWidget(self.learnLeftPanelTop)
+        self.learnLeftPanelSettingsButton=QPushButton("Settings")
+        self.learnLeftPanelSettingsButton.setStyleSheet("QPushButton{font: 20pt Comic Sans MS; color: white;border-style: outset; border-width: 2px; border-radius: 10px;border-color:white;} QPushButton::hover{background-color: white; color: black;}")
+        self.learnLeftPanelSettingsButton.clicked.connect(self._showLearnSettings)
+        self.learnLeftPanelLayout.addWidget(self.learnLeftPanelSettingsButton)
+        self.learnRightPanel=LearnCard(self)
+        self.learnFrameLayout.addWidget(self.learnRightPanel)
+###########################################################################################################################
+#######Learn Code##########################################################################################################
+###########################################################################################################################
     def Test(self):pass
     def _editStudySet(self):
         self.EDIT=True
@@ -534,6 +781,9 @@ class MainWindow(QStackedWidget):
             self.table.insertRow(self.table.rowCount())
             self.table.setItem(self.table.rowCount()-1,0,QTableWidgetItem(file.split("/")[-1].split(".")[0]))
 if __name__ == "__main__":
+    if not system()=="Windows":
+        print("This program is only supported on Windows")
+        sys.exit(-1)
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
